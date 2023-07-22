38c7b62fed23a4612a39529754db21b4595b21e7
alejandroc45 <126873159+alejandrocanosa45@users.noreply.github.com>
2023-07-22 20:22:23 +0200

    pruebas secuencial

--- Test Suites/Pruebas de rendimiento/Pruebas de rendimiento.ts
+++ Test Suites/Pruebas de rendimiento/Pruebas de rendimiento.ts
@@ -3,8 +3,8 @@
    <description></description>
    <name>Pruebas de rendimiento</name>
    <tag></tag>
-   <delayBetweenInstances>10</delayBetweenInstances>
-   <executionMode>PARALLEL</executionMode>
+   <delayBetweenInstances>16</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>2</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
